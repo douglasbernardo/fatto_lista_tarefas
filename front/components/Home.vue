924571723c35f15e185132b7783f9cda97d72ab0
Douglas Melo <douglas@emiolo.com>
2024-10-29 19:17:04 -0300

    criacao da tabela e insercao dos icones e botao de nova tarefa

---
+++ front/components/Home.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <ListTable/>
+    </div>
+</template>
---
+++ front/components/ListTable.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-container>
+    <div class="bg-grey text-center text-h4 ma-2 pa-2">Lista de Tarefas</div>
+    <v-btn class="bg-blue ma-2" prepend-icon="mdi-plus-circle" variant="text">Adicionar Nova Tarefa</v-btn>
+    <v-table class="ma-2">
+      <thead class="bg-grey">
+        <tr>
+          <th class="text-left" v-for="item in tableHeaders" v-text="item" />
+          <th colspan="2">Ações</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="item in desserts"
+          :key="item.name"
+          :class="{'bg-yellow-lighten-3': item.cost > 1000}"
+        >
+          <td>#{{ item.id }}</td>
+          <td>{{ item.name }}</td>
+          <td>R$ {{ item.cost }}</td>
+          <td>{{ item.limitDate }}</td>
+          <td>
+            <v-btn icon="mdi-pencil" variant="text"></v-btn>
+            <v-btn icon="mdi-delete" variant="text" color="red"></v-btn>
+          </td>
+        </tr>
+      </tbody>
+    </v-table>
+  </v-container>
+</template>
+<script setup lang="ts">
+  const tableHeaders = ref(['id','Nome da Tarefa','Custo','Data Limite'])
+  const desserts = ref([
+    {
+        id: 1,
+        name: 'Criar um sistema',
+        cost: 14000,
+        limitDate: '14/10/2025'
+    },
+    {
+        id: 2,
+        name: 'Criar um pdf',
+        cost: 70,
+        limitDate: '11/10/2024'
+    },
+    {
+        id: 2,
+        name: 'Criar um TCC',
+        cost: 1370,
+        limitDate: '20/11/2024'
+    }
+  ])
+</script>
